372e01c4072ea406e5886562d586c18c2901c6e9
Durjay Ghosh <42237029+durjaygp@users.noreply.github.com>
2026-05-14 00:48:58 +0600

    update

--- app.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from '@tanstack/solid-start/config'
-
-export default defineConfig({
-  server: {
-    preset: 'vercel',
-  },
-})
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackStart } from '@tanstack/solid-start/plugin/vite'
+import solidPlugin from 'vite-plugin-solid'
 
 export default defineConfig({
   plugins: [
     devtools(),
     tailwindcss(),
     tanstackStart(),
+    solidPlugin({ ssr: true }),
   ],
 })
